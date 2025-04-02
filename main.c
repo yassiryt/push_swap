42c8e362453c1f4b1b97d37e24a1a8022cae84fc
yassiryt <yassir.tana@gmail.com>
2025-04-02 06:40:35 +0000

    first

--- main.c
+++ main.c
@@ -1,26 +1,35 @@
-
 #include "push_swap.h"
 
-void quit(t_stab *va, int c, char **v)
+static void	free_string_array(char **v)
 {
-    if (c == 1 || c == 5)
-        ft_putstr("Error\n");
-    if (c != 2 && c != 5) {
-        if (va->tab_a)
-            free(va->tab_a);
-        if (va->tab_b)
-            free(va->tab_b);
-        if (v)
+	int	i;
+
+	i = 0;
+	if (v)
+	{
+		while (v[i])
 		{
-            int i = 0;
-            while (v[i]) {
-                free(v[i]);
-                i++;
-            }
-            free(v);
-        }
-    }
-    exit(0);
+			free(v[i]);
+			i++;
+		}
+		free(v);
+	}
+}
+
+void	quit(t_stab *va, int c, char **v)
+{
+	if (c == 1 || c == 5)
+		ft_putstr("Error\n");
+	if (c != 2 && c != 5)
+	{
+		if (va->tab_a)
+			free(va->tab_a);
+		if (va->tab_b)
+			free(va->tab_b);
+		if (v)
+			free_string_array(v);
+	}
+	exit(0);
 }
 
 void	param_to_var(t_stab *var, char **v, int c)
@@ -50,8 +59,8 @@ char	**parc(char **v, char **tmp, int *c)
 	int		i;
 	char	*tab;
 
-	tab = NULL;
 	i = 1;
+	tab = NULL;
 	if (check_space(v) == 0)
 		quit(0, 5, 0);
 	while (v[i])
@@ -60,48 +69,48 @@ char	**parc(char **v, char **tmp, int *c)
 		tab = ft_strjoin(tab, "  ");
 	}
 	tmp = ft_split(tab, ' ');
-	i = 0;
 	free(tab);
 	if (!tmp)
-		exit (0);
+		exit(0);
+	i = 0;
 	while (tmp[i])
 		i++;
 	*c = i;
 	return (tmp);
 }
 
-void	nor(t_stab *var)
+static void	handle_small_sort(t_stab *var, int c)
 {
-	indexing(var);
-	push_sorted_to_b(var);
-	push_sorted_to_a(var);
+	if (c <= 4)
+		sort_3nb(var, c);
+	else if (c > 4)
+		sort_5nb(var, c);
+	quit(var, 0, 0);
 }
 
-int	main(int c, char **v)
+static void	init_and_validate(t_stab *var, char ***tmp, int *c, char **v)
 {
-	t_stab	var = {0};
+	*tmp = parc(v, *tmp, c);
+	if (check_arg(*tmp) == 0)
+		quit(var, 1, *tmp);
+	param_to_var(var, *tmp, (*c)++);
+	if (check_repeat(var->tab_a, var->top_a) == 0)
+		quit(var, 1, 0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stab	var;
 	char	**tmp;
 
+	var = (t_stab){0};
 	tmp = NULL;
-	qu(c);
-	tmp = parc(v, tmp, &c);
-	if (check_arg(tmp) == 0)
-		quit(&var, 1, tmp);
-	if (check_arg(tmp) == 0)
-		quit(&var, 1, tmp);
-	param_to_var(&var, tmp, c++);
-	if (check_repeat(var.tab_a, var.top_a) == 0)
-		quit(&var, 1, 0);
+	qu(argc);
+	init_and_validate(&var, &tmp, &argc, argv);
 	if (check_if_sorted(var.tab_a, var.top_a) == 0)
 		quit(&var, 0, 0);
-	if (c >= 3 && c <= 6)
-	{
-		if (c <= 4)
-			sort_3nb(&var, c);
-		if (c > 4)
-			sort_5nb(&var, c);
-		quit(&var, 0, 0);
-	}
+	if (argc >= 3 && argc <= 6)
+		handle_small_sort(&var, argc);
 	nor(&var);
 	quit(&var, 2, tmp);
 }
